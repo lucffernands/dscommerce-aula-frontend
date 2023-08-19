2e13986e2d6dbfe57375815b89f30583b2c4aa54
Luciano Fernandes <lucianoffernands@gmail.com>
2023-08-19 10:31:01 -0300

    Adding React Router DOM

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -1,13 +1,10 @@
 import './styles.css';
-import HeaderClient from '../../components/HeaderClient';
-import SearchBar from '../../components/SearchBar';
-import CatalogCard from '../../components/CatalogCard';
-import ButtonNextPage from '../../components/ButtonNextPage';
+import SearchBar from '../../../components/SearchBar';
+import CatalogCard from '../../../components/CatalogCard';
+import ButtonNextPage from '../../../components/ButtonNextPage';
 
 export default function Catalog() {
     return (
-        <>
-            <HeaderClient />
             <main>
                 <section id="catalog-section" className="dsc-container">
                    <SearchBar />
@@ -29,6 +26,5 @@ export default function Catalog() {
                         <ButtonNextPage />
                 </section>
             </main>
-        </>
     );
 }
---
+++ src/routes/ClientHome/ProductDateails/index.tsx
@@ -0,0 +1,41 @@
+import './styles.css';
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import { ProductDTO } from '../../../models/products';
+
+const product: ProductDTO = {
+    id: 2,
+    name: "Smart TV",
+    description: "Esta TV é muito bonita",
+    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
+    price: 2500.99,
+    categories: [
+        {
+            id: 2,
+            name: "Eletronicos"
+        },
+        {
+            id: 3,
+            name: "Computadores"
+        },
+        {
+            id: 3,
+            name: "Importados"
+        }
+    ]
+}
+
+export default function ProductDetails() {
+    return (
+        <main>
+            <section id="product-details-section" className="dsc-container">
+                <ProductDetailsCard product={product} />
+                <div className="dsc-btn-page-container">
+                    <ButtonPrimary />
+                    <ButtonInverse />
+                </div>
+            </section>
+        </main>
+    );
+}

d347c40d9d8181826147f9eb204f8f9559b598a2
Luciano Fernandes <lucianoffernands@gmail.com>
2023-08-29 22:06:48 -0300

    Catalog next page

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -6,21 +6,38 @@ import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/product';
 import * as productService from '../../../services/product-service';
 
+type QueryParams = {
+    page: number;
+    name: string;
+}
+
 export default function Catalog() {
+
+    const [isLastPage, setIsLastPage] = useState(false);
 
     const [products, setProducts] = useState<ProductDTO[]>([]);
 
-    const [productName, setProductName] = useState("");
+    const [queryParams, setQueryParams] = useState<QueryParams>({
+        page: 0,
+        name: ""
+    });
 
     useEffect(() => {
-        productService.findPageRequest(0, productName)
+        productService.findPageRequest(queryParams.page, queryParams.name)
             .then(response => {
-                setProducts(response.data.content);
+                const nextPage = response.data.content;
+                setProducts(products.concat(nextPage));
+                setIsLastPage(response.data.last);
             })
-    }, [productName]);
+    }, [queryParams]);
 
     function handleSearch(searchText: string) {
-        setProductName(searchText);
+        setProducts([]);
+        setQueryParams({...queryParams, page: 0, name: searchText});
+    }
+
+    function handleNextPageClick() {
+        setQueryParams({...queryParams, page: queryParams.page + 1});
     }
 
     return (
@@ -35,8 +52,14 @@ export default function Catalog() {
                             )
                         }
                     </div>  
-                      
-                        <ButtonNextPage />
+                    
+                    {
+                        !isLastPage &&
+                        <div onClick={handleNextPageClick}>
+                            <ButtonNextPage />
+                        </div>
+                    }
+                        
                 </section>
             </main>
     );
